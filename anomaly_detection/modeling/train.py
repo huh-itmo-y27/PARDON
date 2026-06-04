382abbdba030ab07672d9d7661fd4e4f4bc38e63
Georgii Lopatenko <81328772+Lopa10ko@users.noreply.github.com>
2026-06-04 13:21:20 +0300

    fix (cr): do not merge false alarms across true anomaly windows

--- anomaly_detection/modeling/train.py
+++ anomaly_detection/modeling/train.py
@@ -159,8 +159,11 @@ def compute_false_alarms_per_day(
     num_events = 1
     for prev_idx, cur_idx in zip(fp_idx[:-1], fp_idx[1:]):
         delta_minutes = (
-            timestamps.iloc[cur_idx] - timestamps.iloc[prev_idx]
-        ).total_seconds() / 60.0
+    for prev_idx, cur_idx in zip(fp_idx[:-1], fp_idx[1:], strict=False):
+        if np.any(y_true[prev_idx + 1 : cur_idx] == 1):
+            num_events += 1
+            continue
+        delta_minutes = (timestamps.iloc[cur_idx] - timestamps.iloc[prev_idx]).total_seconds() / 60.0
         if delta_minutes > merge_gap_minutes:
             num_events += 1
     normal_mask = y_true == 0
