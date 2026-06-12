9e451e7edca4a3c561098000ae7ec8bb83e749ed
Romankk03 <126852895+Romankk03@users.noreply.github.com>
2026-06-12 17:35:08 +0300

    feat: added argo cd pods (grafana, prometheus, mlflow)

--- scripts/k8s-port-forward.sh
+++ scripts/k8s-port-forward.sh
@@ -4,8 +4,16 @@ set -eu
 NAMESPACE="${K8S_NAMESPACE:-pardon}"
 API_LOCAL_PORT="${API_LOCAL_PORT:-8000}"
 UI_LOCAL_PORT="${UI_LOCAL_PORT:-3001}"
+GRAFANA_LOCAL_PORT="${GRAFANA_LOCAL_PORT:-3000}"
+MLFLOW_LOCAL_PORT="${MLFLOW_LOCAL_PORT:-5000}"
+PROMETHEUS_LOCAL_PORT="${PROMETHEUS_LOCAL_PORT:-9090}"
+PUSHGATEWAY_LOCAL_PORT="${PUSHGATEWAY_LOCAL_PORT:-9091}"
 API_REMOTE_PORT="${API_REMOTE_PORT:-8000}"
 UI_REMOTE_PORT="${UI_REMOTE_PORT:-3001}"
+GRAFANA_REMOTE_PORT="${GRAFANA_REMOTE_PORT:-3000}"
+MLFLOW_REMOTE_PORT="${MLFLOW_REMOTE_PORT:-5000}"
+PROMETHEUS_REMOTE_PORT="${PROMETHEUS_REMOTE_PORT:-9090}"
+PUSHGATEWAY_REMOTE_PORT="${PUSHGATEWAY_REMOTE_PORT:-9091}"
 RETRY_SECONDS="${PORT_FORWARD_RETRY_SECONDS:-2}"
 
 PIDS=""
@@ -46,6 +54,18 @@ forward_loop "api" "pardon-api" "$API_LOCAL_PORT" "$API_REMOTE_PORT" "pardon-api
 PIDS="$PIDS $!"
 
 forward_loop "ui" "pardon-ui" "$UI_LOCAL_PORT" "$UI_REMOTE_PORT" "pardon-ui" &
+PIDS="$PIDS $!"
+
+forward_loop "grafana" "pardon-grafana" "$GRAFANA_LOCAL_PORT" "$GRAFANA_REMOTE_PORT" "pardon-grafana" &
+PIDS="$PIDS $!"
+
+forward_loop "mlflow" "pardon-mlflow" "$MLFLOW_LOCAL_PORT" "$MLFLOW_REMOTE_PORT" "pardon-mlflow" &
+PIDS="$PIDS $!"
+
+forward_loop "prometheus" "pardon-prometheus" "$PROMETHEUS_LOCAL_PORT" "$PROMETHEUS_REMOTE_PORT" "pardon-prometheus" &
+PIDS="$PIDS $!"
+
+forward_loop "pushgateway" "pardon-pushgateway" "$PUSHGATEWAY_LOCAL_PORT" "$PUSHGATEWAY_REMOTE_PORT" "pardon-pushgateway" &
 PIDS="$PIDS $!"
 
 echo "Port forwards are running. Press Ctrl+C to stop."
